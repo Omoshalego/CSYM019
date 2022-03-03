2725355a72072774ded93a5477658049828be517
martin <omoshalego@gmail.com>
2022-03-03 16:51:39 +0000

    variableChange

--- week4/week4_c.js
+++ week4/week4_c.js
@@ -2,8 +2,9 @@
 function myLoadFunction() {
     var headings = document.getElementById('heading');
     var paragraph = document.getElementById('paragraph');
-    heading.firstChild.nodeValue = 'New Heading';
+    headings.firstChild.nodeValue = 'New Heading';
     paragraph.firstChild.nodeValue = 'New Paragraph';
    }
+   
    document.addEventListener('DOMContentLoaded', myLoadFunction);
   
