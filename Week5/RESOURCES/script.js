67e3ab5b0695702489336abbf11af17172bfdcb5
martin <omoshalego@gmail.com>
2022-03-09 03:35:44 +0000

    Week5Exercise

---
+++ Week5/RESOURCES/script.js
@@ -0,0 +1,21 @@
+  
+//    Download Topic2-Exercise1.zip and extract it 
+//    2) Add a <script> tag to the page so you can run some javascript
+//    3) Add an event listener for DOMContentLoaded and a function that 
+//   it runs when the page loads
+//   4) In the load function you created, add a click event to the 
+//   element with the ID `circle` so that when the circle is clicked an 
+//   alert box appears and says “The button was pressed”
+//    Note: You will need this for the rest of today's exercise
+  
+function myClickFunction(){
+ alert('The button was pressed !')
+
+   }
+
+function myLoadFunction() {
+    var element = document.getElementById('circle');
+    element.addEventListener('click', myClickFunction);
+
+   }
+   document.addEventListener('DOMContentLoaded', myLoadFunction);
